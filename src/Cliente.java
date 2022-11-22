0ac7b2b5427d7508c2144fc90e9a4269e3fad618
asafeamancio <asafeamancio@gmail.com>
2022-11-22 15:22:29 -0300

    novo update

---
+++ src/Cliente.java
@@ -0,0 +1,31 @@
+public class Cliente {
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    private int cpf;
+    private String nome;
+    private String profissao;
+
+}
